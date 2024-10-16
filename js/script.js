3cd5be4dff53eda6127396e6bcc5b6794dc503f4
Gabriel <gbri2586@gmail.com>
2024-10-16 19:30:46 -0300

    Ajuste na nav para mobile

--- js/script.js
+++ js/script.js
@@ -2,8 +2,3 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("#header") 
     header.classList.toggle("rolagem", window.scrollY > 60)
 })
-
-const hamburguer = document.querySelector(".hamburguer")
-const nav = document.querySelector(".nav")
-
-hamburguer.addEventListener("click" , () => nav.classList.toggle("active"))
