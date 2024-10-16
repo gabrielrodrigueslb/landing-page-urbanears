9bc0444a53f0eef05b1c69ed94b0bc05fe8b49af
Gabriel <gbri2586@gmail.com>
2024-10-16 11:34:51 -0300

    Inicio de adaptação no media querie

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("rolagem", window.scrollY > 60)
 })
 
+const hamburguer = document.querySelector(".hamburguer")
+const nav = document.querySelector(".nav")
+
+hamburguer.addEventListener("click" , () => nav.classList.toggle("active"))
