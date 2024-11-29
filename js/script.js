57fcb4f9e1f499101b554a08dd7084cbe1195660
Gabriel <gbri2586@gmail.com>
2024-11-28 23:33:49 -0300

    Correção de quebra de layout

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector("#header") 
     header.classList.toggle("rolagem", window.scrollY > 60)
 })
+
+const fone = document.querySelector(".fone")
+
+const myObserver = new IntersectionObserver((entries) =>{
+    console.log(entries)
+})
+
+myObserver.observe(fone)
