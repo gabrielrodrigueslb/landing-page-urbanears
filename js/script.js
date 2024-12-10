b57e447a2209819df1e872169e67fab816cd5eab
Gabriel <gbri2586@gmail.com>
2024-12-10 13:28:18 -0300

    Adicionado menu hamburguer

--- js/script.js
+++ js/script.js
@@ -3,10 +3,27 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("rolagem", window.scrollY > 60)
 })
 
-const fone = document.querySelector(".fone")
+// const fone = document.querySelector(".fone")
 
-const myObserver = new IntersectionObserver((entries) =>{
-    console.log(entries)
-})
+// const myObserver = new IntersectionObserver((entries) =>{
+//     console.log(entries)
+// })
 
-myObserver.observe(fone)
+// myObserver.observe(fone)
+
+
+
+// menu hamburguer
+const hamburguer = document.querySelector(".hamburguer")
+const nav = document.querySelector(".navbar")
+const navItems = document.querySelectorAll(".menu a");
+
+hamburguer.addEventListener('click', () =>
+    nav.classList.toggle("active")
+)
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.remove("active"); // Remove a classe 'active' para fechar o menu
+    });
+});
